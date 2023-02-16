856d5ee9271bb7c21000a38db250fe4c6655c266
Your Nameobinna <you@example.comobinna.agbo1@gmail.com>
2023-02-16 15:01:43 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
- * Size of various types
- * main Entry
+ * main - Entry
  * Return: Always 0
  */
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)\n");
-      	printf("Size of an int: 4 byte(s)\n");
-	printf("Size of a long int: 4 byte(s)\n");
-	printf("Size of a long long int: 8 byte(s)\n");
-	printf("Size of a float: 4 byte(s)\n");
+	printf("size of a char :1 byte(s)\n");
+	printf("size of an int: 4 byte(s)\n");
+	printf("size of a long int : 4 byte(s)\n");
+	printf("size of a long long int : 8 byte(s)\n");
+	printf("size of a float : 4 byte(s)\n");
 	return (0);
 }
