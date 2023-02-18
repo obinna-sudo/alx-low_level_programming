9469ff120bc78e18cf2a3fcba3d6c2b0ed1e77fe
Your Nameobinna <you@example.comobinna.agbo1@gmail.com>
2023-02-18 05:01:53 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet in reverse order 
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'z'; lc <= 'a'; lc--)
+		putchar(lc);
+
+	putchar('\n');
+
+	return (0);
+}
