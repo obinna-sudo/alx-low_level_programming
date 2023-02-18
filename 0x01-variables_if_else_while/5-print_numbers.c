f2adc9cdc59c9e323b55ddcce15a7ebcad2512ec
Your Nameobinna <you@example.comobinna.agbo1@gmail.com>
2023-02-18 03:14:53 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints numbers from 0 to 9
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	for (int i = 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
