ac6bc4fdfb9a88bbc2cf564c62d35acdee45eabb
Your Nameobinna <you@example.comobinna.agbo1@gmail.com>
2023-02-17 16:12:38 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,29 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* 
- * main - Program that compares postivity and neagtivity of a number 
+
+/**
+ * main - prints postive or negative value
  * Return: Always 0
  */
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is postive\n", n);
 	}
-	else if (n == 0)
+	else if (n == 0}
 	{
-		print("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
-		print("%d is negative\n", n);
+	{
+		printf("%d is negative\n", n);
+	}
 
 	return (0);
-}
