c5541a699c2caa8768e44c5b609ae45a78ab3fad
Your Nameobinna <you@example.comobinna.agbo1@gmail.com>
2023-02-22 09:02:05 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
-
 /**
- * main - entry point 
- * Return : Always 0 (success)
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 
-int main (void)
+int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
+
