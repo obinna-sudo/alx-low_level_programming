de3f243e78543674e1c43ed35b82170973d360ad
Your Nameobinna <you@example.comobinna.agbo1@gmail.com>
2023-03-16 17:03:03 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *
+* comments
+
+* *_memset - fills memory with a constant byte
+
+* @s: memory area to be filled
+
+* @b: char to copy
+
+* @n: number of times to copy b
+
+*
+
+* Return: pointer to the memory area s
+
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+
+	unsigned int i;
+
+
+	for (i = 0; i < n; i++)
+
+	{
+
+		s[i] = b;
+
+	}
+
+
+
+	return (s);
+
+}
